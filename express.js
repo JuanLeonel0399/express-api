3720ea70ca33a02ecc032e7827962313ff71a678
Juan Leonel Mitre <juanleonel.developer@gmail.com>
2024-07-31 12:36:08 -0500

    modified:   express.js

--- express.js
+++ express.js
@@ -2,7 +2,7 @@ const express = require('express');
 const crypto = require('node:crypto');
 const cors = require('cors');
 const movies = require('./movies.json');
-const { validateMovie, validatePartialMovie } = require('./schema');
+const { validateMovie, validatePartialMovie } = require('./schema')
 const app = express();
 
 app.use(express.json())
